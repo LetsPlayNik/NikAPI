a269b057b06fa6cdf13d3d7e6915c3a0f77f0e5c
LetsPlayNik <niklasst2102@gmail.com>
2021-12-29 21:17:05 +0000

    adds paginated inventory

--- src/main/java/de/niklas/api/spigot/inventory/PaginatedInventoryMenu.java
+++ src/main/java/de/niklas/api/spigot/inventory/PaginatedInventoryMenu.java
@@ -6,9 +6,25 @@ package de.niklas.api.spigot.inventory;
  * Created at 29.12.2021 - 20:10Uhr
  */
 
+import org.bukkit.inventory.ItemStack;
+
 public class PaginatedInventoryMenu extends InventoryMenu {
 
     public PaginatedInventoryMenu(int size, String displayName) {
         super(size, displayName);
+    }
+    public PaginatedInventoryMenu(int size) {
+        super(size);
+    }
+
+    public void setForwardItem(int index, ItemStack itemStack) {
+        setItem(index, itemStack, player -> {
+            System.out.println("Seite vorwärts.");
+        });
+    }
+    public void setBackwardsItem(int index, ItemStack itemStack) {
+        setItem(index, itemStack, player -> {
+            System.out.println("Seite zurück.");
+        });
     }
 }
